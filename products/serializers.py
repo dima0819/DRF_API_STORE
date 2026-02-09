c6d07fef23a7170a53e6d5731c1b482c0608237c
Dima Dauzhuk <dima.dauzhuk@icloud.com>
2026-02-09 19:22:49 +0100

    Implement Product and Category views with serializers and URL routing. Add Search Filters in Views

---
+++ products/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Product, Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description']
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field='name', read_only=True)
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'description', 'price', 'stock', 'created_at', 'category']
+        
+
--- products/views.py
+++ products/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .serializers import ProductSerializer, CategorySerializer
+from .models import Product, Category
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import generics, filters
 
-# Create your views here.
+class ProductViewList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name', 'description', 'category__name']
+    ordering_fields = ['price', 'created_at']
+    
+class ProductViewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+class CategoryViewList(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+class CategoryViewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
