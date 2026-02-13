236c4017894dc67bd8c433d66bede0f28d507de8
Dima Dauzhuk <dima.dauzhuk@icloud.com>
2026-02-13 13:19:14 +0100

    Refactor Product model to include slug auto generation and update serializers to validate price

--- products/models.py
+++ products/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(max_length=50, unique=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.BooleanField(default=True)
@@ -12,6 +13,11 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)[:50]
+            super().save(*args, **kwargs)
     
     class Meta:
         ordering = ['-created_at']
--- products/serializers.py
+++ products/serializers.py
@@ -7,9 +7,12 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'description']
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = serializers.SlugRelatedField(slug_field='name', read_only=True)
+    category = serializers.SlugRelatedField(slug_field='name', queryset=Category.objects.all())
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'price', 'stock', 'created_at', 'category']
         
-
+    def validate_price(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Price must be >= 0")
+        return value
