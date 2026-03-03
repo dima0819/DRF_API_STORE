814d93ad2e9fb1a1d48751bb4dbf9ae02898a565
Dima Dauzhuk <dima.dauzhuk@icloud.com>
2026-03-03 21:32:53 +0100

    Refactor product and category views to use IsAdminOrReadOnly permission and implement pagination for product listings.

--- products/views.py
+++ products/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics, filters
+from rest_framework.pagination import PageNumberPagination
+
 from .serializers import ProductSerializer, CategorySerializer
 from .models import Product, Category
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework import generics, filters
+from .permissions import IsAdminOrReadOnly
+
+class ProductPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 50
+
 
 class ProductViewList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
+    pagination_class = ProductPagination
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['name', 'description', 'category__name']
     ordering_fields = ['price', 'created_at']
@@ -15,14 +24,14 @@ class ProductViewList(generics.ListCreateAPIView):
 class ProductViewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
     
 class CategoryViewList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
     
 class CategoryViewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
